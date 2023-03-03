497fc015261505261a6a90c909769fc865abd74e
Muhammed Emin Emrak <emin.emrak@outlook.com>
2023-03-03 15:44:04 +0300

    service added

---
+++ apiMVVM/Model/CryptoCurrency.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoCurrency.swift
+//  apiMVVM
+//
+//  Created by Eminko on 2.03.2023.
+//
+
+import Foundation
+
+struct CryptoCurrency : Decodable {
+    let currency : String
+    let price : String
+}
---
+++ apiMVVM/Service/Webservice.swift
@@ -0,0 +1,36 @@
+//
+//  Webservice.swift
+//  apiMVVM
+//
+//  Created by Eminko on 2.03.2023.
+//
+
+import Foundation
+
+class Webservice {
+    
+    func downloadCurrencies(url: URL, completion: @escaping ([CryptoCurrency]?) -> ()) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                
+                let cryptoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                
+                print(cryptoList)
+                
+                if let cryptoList = cryptoList {
+                    completion(cryptoList)
+
+                }
+                
+            }
+        }.resume()
+        
+        
+    }
+    
+    
+}
